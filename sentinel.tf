7d1efdbcf7b774d48ee95affa0e8655a39eb4de1
Lauryn James <164093456+laurynjames1@users.noreply.github.com>
2025-07-14 05:07:04 -0500

    Update sentinel.tf

--- sentinel.tf
+++ sentinel.tf
@@ -10,18 +10,4 @@ resource "azurerm_sentinel_log_analytics_workspace_onboarding" "sentinel" {
   workspace_id = azurerm_log_analytics_workspace.sentinel_law.id
 }
 
-resource "azurerm_virtual_machine_extension" "oms_agent" {
-  name                 = "OmsAgentForLinux"
-  virtual_machine_id   = azurerm_linux_virtual_machine.vm-03.id
-  publisher            = "Microsoft.EnterpriseCloud.Monitoring"
-  type                 = "OmsAgentForLinux"
-  type_handler_version = "1.13"
 
-  settings = jsonencode({
-    workspaceId = azurerm_log_analytics_workspace.sentinel_law.workspace_id
-  })
-
-  protected_settings = jsonencode({
-    workspaceKey = azurerm_log_analytics_workspace.sentinel_law.primary_shared_key
-  })
-}
