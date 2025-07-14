c4869a2c679135410bff5873f9797776f5ae3e0a
Lauryn James <164093456+laurynjames1@users.noreply.github.com>
2025-07-14 05:01:12 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,59 +1,128 @@
 provider "azurerm" {
   features {}
-  subscription_id = "cbe41c4f-5119-4011-8aae-b4163df9cc65"
 }
 
-resource "azurerm_resource_group" "ltc-rg" {
+resource "azurerm_resource_group" "ltc_rg" {
   name     = "ltc-resource"
   location = "East US"
 }
 
-
-
-resource "azurerm_virtual_network" "ltc-vn" {
+resource "azurerm_virtual_network" "ltc_vnet" {
   name                = "ltc-network"
-  resource_group_name = azurerm_resource_group.ltc-rg.name
-  location            = azurerm_resource_group.ltc-rg.location
+  resource_group_name = azurerm_resource_group.ltc_rg.name
+  location            = azurerm_resource_group.ltc_rg.location
   address_space       = ["10.30.0.0/16"]
 
   tags = {
-    environment = "dev"
+    environment = "example tag"
   }
 }
 
-
-resource "azurerm_subnet" "ltc-sn" {
+resource "azurerm_subnet" "ltc_subnet" {
   name                 = "ltc-subnet"
-  resource_group_name  = azurerm_resource_group.ltc-rg.name
-  virtual_network_name = azurerm_virtual_network.ltc-vn.name
+  resource_group_name  = azurerm_resource_group.ltc_rg.name
+  virtual_network_name = azurerm_virtual_network.ltc_vnet.name
   address_prefixes     = ["10.30.1.0/24"]
 }
 
-resource "azurerm_network_security_group" "ltc-dev-rule" {
-  name                = "ltc-dev-rule"
-  location            = azurerm_resource_group.ltc-rg.location
-  resource_group_name = azurerm_resource_group.ltc-rg.name
+resource "azurerm_network_security_group" "ltc_nsg" {
+  name                = "ltc-nsg"
+  location            = azurerm_resource_group.ltc_rg.location
+  resource_group_name = azurerm_resource_group.ltc_rg.name
 
   tags = {
-    environment = "dev"
+    environment = "example tag"
   }
 }
 
-resource "azurerm_network_security_rule" "ltc-dev-rule" {
-  name                        = "ltc-dev-rule"
+resource "azurerm_network_security_rule" "ltc_ssh_rule" {
+  name                        = "allow-ssh"
   priority                    = 100
   direction                   = "Inbound"
   access                      = "Allow"
-  protocol                    = "*"
+  protocol                    = "Tcp"
   source_port_range           = "*"
-  destination_port_range      = "*"
-  source_address_prefix       = "172.0.12.230/32"
+  destination_port_range      = "22"
+  source_address_prefix       = "YOUR_PUBLIC_IP/32"
   destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.ltc-rg.name
-  network_security_group_name = azurerm_network_security_group.ltc-dev-rule.name
+  resource_group_name         = azurerm_resource_group.ltc_rg.name
+  network_security_group_name = azurerm_network_security_group.ltc_nsg.name
 }
 
-resource "azurerm_subnet_network_security_group_association" "ltc-sga" {
-  subnet_id                 = azurerm_subnet.ltc-sn.id
-  network_security_group_id = azurerm_network_security_group.ltc-dev-rule.id
+resource "azurerm_network_interface" "ltc_nic" {
+  name                = "ltc-nic"
+  location            = azurerm_resource_group.ltc_rg.location
+  resource_group_name = azurerm_resource_group.ltc_rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.ltc_subnet.id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "10.30.1.10"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "ltc_sga" {
+  subnet_id                 = azurerm_subnet.ltc_subnet.id
+  network_security_group_id = azurerm_network_security_group.ltc_nsg.id
+}
+
+resource "random_id" "unique" {
+  keepers = {
+    resource_group = azurerm_resource_group.ltc_rg.name
+  }
+  byte_length = 8
+}
+
+resource "azurerm_storage_account" "ltc_storage" {
+  name                     = "diag${random_id.unique.hex}"
+  location                 = azurerm_resource_group.ltc_rg.location
+  resource_group_name      = azurerm_resource_group.ltc_rg.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_linux_virtual_machine" "example_vm" {
+  name                  = "example-vm"
+  location              = azurerm_resource_group.ltc_rg.location
+  resource_group_name   = azurerm_resource_group.ltc_rg.name
+  network_interface_ids = [azurerm_network_interface.ltc_nic.id]
+  size                  = "Standard_B1s"
+
+  os_disk {
+    name                 = "ltc-osdisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+
+  computer_name  = "example-vm"
+  admin_username = var.admin_username
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = var.ssh_public_key
+  }
+
+  boot_diagnostics {
+    storage_account_uri = azurerm_storage_account.ltc_storage.primary_blob_endpoint
+  }
+}
+
+resource "azurerm_log_analytics_workspace" "ltc_law" {
+  name                = "ltc-law"
+  location            = azurerm_resource_group.ltc_rg.location
+  resource_group_name = azurerm_resource_group.ltc_rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_sentinel_log_analytics_workspace_onboarding" "ltc_sentinel" {
+  workspace_id = azurerm_log_analytics_workspace.ltc_law.id
 }
