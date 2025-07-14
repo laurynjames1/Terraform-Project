333902458694c299e20feb3143cfca0fa5277f4d
Lauryn James <164093456+laurynjames1@users.noreply.github.com>
2025-07-14 05:12:05 -0500

    Update virtual_machine.tf

--- virtual_machine.tf
+++ virtual_machine.tf
@@ -1,11 +1,11 @@
 # Create virtual machine
 
-resource "azurerm_linux_virtual_machine" "vm-03" {
-  name                  = "vm-03"
+resource "azurerm_linux_virtual_machine" "example-vm-name" {
+  name                  = "example-vm-name"
   location              = azurerm_resource_group.ltc-rg.location
   resource_group_name   = azurerm_resource_group.ltc-rg.name
   network_interface_ids = [azurerm_network_interface.ltc-NIC.id]
-  size                  = "Standard_DS1_v2"
+  size                  = "Standard_DS1"
   }
   
   os_disk {
@@ -21,12 +21,12 @@ resource "azurerm_linux_virtual_machine" "vm-03" {
     version   = "latest"
   }
 
-  computer_name  = "Asterick001"
-  admin_username = var.admin_username
+  computer_name  = "Example-name"
+  admin_username = var.admin_username #This variable will be found in your "Variables.tf" folder
   
   admin_ssh_key {
     username = var.admin_username
-    public_key = file("/Users/jlohackx/.ssh/id_rsa.pub")
+    public_key = file("Public-key") #Insert file path after generating public key locally 
   }
 
   boot_diagnostics {
