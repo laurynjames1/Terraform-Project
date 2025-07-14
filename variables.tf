babbf71d2070f4f6a846548b3705b20bf1c9ac5f
Lauryn James <164093456+laurynjames1@users.noreply.github.com>
2025-07-14 05:34:41 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,15 @@
+# Defines what variables your Terraform project expects.
+# Sets types, descriptions, and optional default values.
+
+# Example:
+
+variable "admin_username" {
+  type        = string
+  description = "Admin username for the virtual machine"
+}
+
+variable "ssh_public_key" {
+  type        = string
+  description = "SSH public key for VM login"
+}
 
